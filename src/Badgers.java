b90a29b940490b80a9e707f0cbdac402523c14e0
ilabstaff <ilabstaff@172.16.116.79>
2019-06-24 18:00:07 -0700

    badgers

---
+++ src/Badgers.java
@@ -0,0 +1,56 @@
+import javax.swing.JOptionPane;
+
+public class Badgers {
+	
+
+	public static void main(String[] args) {
+		
+		
+	
+		
+	for (int j = 0; j < 2; j++) {
+
+		String behzad = "badgers";
+		String moon = "mushroom";
+		String star = "snake";
+		
+		
+		for (int i = 0; i < 8; i++)  {
+		System.out.println(behzad);
+	    }
+		 for (int i = 0; i < 2; i++) {
+			System.out.println(moon);
+		}
+		for (int i = 0; i < 5; i++)  {
+			System.out.println(star);
+		}
+	
+	    }
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+}
+}
---
+++ src/my_ages.java
@@ -0,0 +1,28 @@
+import javax.lang.model.element.VariableElement;
+import javax.swing.JOptionPane;
+
+public class my_ages {
+public static void main(String[] args) {
+        int behzad = 1;
+	for (int i = 0; i < 17; i++) {
+		JOptionPane.showMessageDialog(null, behzad++);
+		System.out.println(i);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
